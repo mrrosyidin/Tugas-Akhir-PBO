3c69d5596eeb22cef9d27c29699f892e8c640c1e
mrrosyidin <mrrosyidin31@gmail.com>
2023-01-03 12:03:40 +0700

    first commit

--- src/views/Transaction/DetailTransaction/TambahDetailTransactionPage.java
+++ src/views/Transaction/DetailTransaction/TambahDetailTransactionPage.java
@@ -1,8 +1,21 @@
 package views.Transaction.DetailTransaction;
 
+import java.util.Scanner;
+
 public class TambahDetailTransactionPage {
+    Scanner input = new Scanner(System.in);
+
     public void tambahDetailTransaction(int code){
-        System.out.println("Masukkan nama atau nomorseri barang : ");
-        System.out.println("Masukkan jumlah barang : ");
+        String loop;
+
+        do{
+            System.out.print("Masukkan nama barang atau code barang : ");
+            System.out.print("Masukkan jumlah barang : ");
+
+
+            System.out.print("Apakah ingin menambah list lagi (y/n)? ");
+            loop = input.next(); 
+            input.nextLine();
+        }while(loop == "y" || loop == "Y");
     }
 }
